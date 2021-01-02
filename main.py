4350d2ad0d47f7d37bbc61919df7175f5e093bc3
chenjj100419 <61618153+chenjj100419@users.noreply.github.com>
2021-01-02 17:38:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,27 @@ class CoolVMTerminaler(object):
     def __init__(self):
         pass
 
+    def init(self):
+        pass
+
+    def run(self):
+        pass
+
 class CoolVMTerminalerAPI(CoolVMTerminaler):
     def __init__(self):
         super(CoolVMTerminalerAPI, self).__init__()
+
+    def init(self):
+        pass
+
+    def run(self):
+        pass
+
+
+if __name__ == "__main__":
+    app = CoolVMTerminaler()
+    app_api = CoolVMTerminalerAPI()
+    app.init()
+    app_api.init()
+    app.run()
+    app_api.run()
